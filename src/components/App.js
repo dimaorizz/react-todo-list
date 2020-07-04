5ab3534475bb35edae4fa80c642ec57143b15c00
Orizz <dimaorizz@gmail.com>
2020-07-05 01:54:29 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>Hello React</div>
+        )
+    }
+}
+
+export default App
