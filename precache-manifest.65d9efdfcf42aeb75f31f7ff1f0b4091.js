8e8f577d6bdf279944d53174f2d2036d20f60bf9
Orizz <dimaorizz@gmail.com>
2020-07-05 15:42:32 +0300

    Updates

---
+++ precache-manifest.65d9efdfcf42aeb75f31f7ff1f0b4091.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae43bd35c970b5e9892d0e786b2fcdf",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "e869ea5d989d21e6a562",
+    "url": "/react-todo-list/static/js/2.95e0f638.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.95e0f638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5738408640eeb7687ce",
+    "url": "/react-todo-list/static/js/main.166660ab.chunk.js"
+  },
+  {
+    "revision": "2c6e5d8f9eaaedf40455",
+    "url": "/react-todo-list/static/js/runtime-main.f23babde.js"
+  }
+]);
